f4977d530c21f58e82f0b7c94347172a0b8b2a47
juliengriffoul <julien.griffoul@viacesi.fr>
2019-09-24 11:29:19 +0200

    :pencil2: Start Authenticator implementation

--- src/main/java/fr/cesi/filedecrypt/controller/Authenticator.java
+++ src/main/java/fr/cesi/filedecrypt/controller/Authenticator.java
@@ -2,4 +2,10 @@ package fr.cesi.filedecrypt.controller;
 
 public class Authenticator {
 
+    public Authenticator() {}
+
+    public boolean login(String username, String password) {
+        return false;
+    }
+
 }
