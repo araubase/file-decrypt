34cf25a4327ae45a162066b5b7be904aa58510b7
juliengriffoul <julien.griffoul@viacesi.fr>
2019-09-24 21:35:43 +0200

    :pencil2: Implement unit tests

---
+++ src/test/java/fr/cesi/filedecrypt/model/DecryptTest.java
@@ -0,0 +1,18 @@
+package fr.cesi.filedecrypt.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DecryptTest {
+
+    @Test
+    public void decrypt() {
+        Decrypt decrypt = new Decrypt();
+        String result = "test";
+        String key = "clé";
+        String s = decrypt.decrypt(result, key);
+
+        assertEquals(result, decrypt.decrypt(s, key));
+    }
+}
